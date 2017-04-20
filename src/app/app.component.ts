62805da642f9c53fbd577a9fb3ab99dfe4ab4a11
Don Burgess <donburgess@Dons-MacBook-Pro.local>
2017-04-20 19:48:51 -0400

    demo app

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'maestro-app',
+    templateUrl: './app.component.html',
+})
+export class AppComponent { }
---
+++ src/app/app.module.tns.ts
@@ -0,0 +1,21 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule
+    ],
+    declarations: [
+        AppComponent
+    ],
+    providers: [
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
