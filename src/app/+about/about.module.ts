967ec4d68a0947e321c7298917781fb590d85bd8
Sebastian Witalec <sebawita@gmail.com>
2017-06-15 17:07:27 +0100

    Replacing SHARED_MODULES (array) with SharedModule (@NgModule)

--- src/app/+about/about.module.ts
+++ src/app/+about/about.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// vendor dependencies
+import { TranslateModule } from '@ngx-translate/core';
+// app
 import { AboutComponent } from './components/about/about.component';
-
-import { SHARED_MODULES, COMPONENT_DECLARATIONS } from './about.common';
+import { AboutRoutes } from './about.routes';
+// common
+import { SharedModule, RouterModule } from '../shared';
 
 @NgModule({
     imports: [
-        ...SHARED_MODULES,
+        SharedModule,
+
+        RouterModule.forChild(<any>AboutRoutes),
+        TranslateModule.forChild()
     ],
     declarations: [
-        ...COMPONENT_DECLARATIONS
+        AboutComponent
     ]
 })
 export class AboutModule { }
