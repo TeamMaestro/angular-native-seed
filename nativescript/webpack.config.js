4346190f3aac29620afb0e80d59a808ed167a386
Sean Perkins <sean@meetmaestro.com>
2017-08-22 17:02:52 -0400

    Update dependencies and underlying webpack config change #44

--- nativescript/webpack.config.js
+++ nativescript/webpack.config.js
@@ -49,7 +49,10 @@ module.exports = env => {
             modules: [
                 "node_modules/tns-core-modules",
                 "node_modules",
-            ]
+            ],
+            alias: {
+                '~': resolve("./app")
+            }
         },
         node: {
             // Disable node shims that conflict with NativeScript
