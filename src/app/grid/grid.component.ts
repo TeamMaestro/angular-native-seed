21b976da866f8bf3b8657af23b5acea54f261283
Svetlin Kozhuharov <kozhuhar@progress.com>
2017-08-31 18:16:15 +0300

    apply styles for web

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { products } from './products';
 
 @Component({
   moduleId: module.id,
   selector: 'seed-grid',
   templateUrl: './grid.component.html',
-  //styleUrls: ['../../../node_modules/@progress/kendo-theme-default/dist/all.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./grid.component.scss']
 })
 export class GridComponent implements OnInit {
 
